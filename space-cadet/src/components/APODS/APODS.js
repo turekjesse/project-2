3ad364f09223ff4b917d784994ee4704af4172c4
turekjesse <turekjesse@gmail.com>
2021-07-02 23:09:43 -0400

    console log removed

--- space-cadet/src/components/APODS/APODS.js
+++ space-cadet/src/components/APODS/APODS.js
@@ -30,8 +30,6 @@ export default function APODS () {
         )
     }
 
-    console.log(apods)
-
     const breakpointColumnsObj = {
         default: 4,
         1100: 3,
@@ -40,46 +38,44 @@ export default function APODS () {
       }
     
     return (
-            <Masonry breakpointCols={breakpointColumnsObj} className="masonry-grid apods-container" columnClassName="masonry-grid_column"> 
-                {
-                    apods.slice(0).reverse().map((apod, idx) => {
-                        const videoEmbed = (
-                            <div className="video-responsive">
-                                <iframe
-                                    width="853"
-                                    height="480"
-                                    src={apod.url}
-                                    frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen
-                                    title={apod.title}
-                                />
+        <Masonry breakpointCols={breakpointColumnsObj} className="masonry-grid apods-container" columnClassName="masonry-grid_column"> 
+            {
+                apods.slice(0).reverse().map((apod, idx) => {
+                    const videoEmbed = (
+                        <div className="video-responsive">
+                            <iframe
+                                width="853"
+                                height="480"
+                                src={apod.url}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title={apod.title}
+                            />
+                        </div>
+                    )
+                    let mediaDisplay;
+                    if (apod.media_type === "video"){
+                        mediaDisplay = videoEmbed
+                    } else {
+                        mediaDisplay = <img className="card-image" src={apod.url} alt={apod.title}/>
+                    }
+                        return (
+                            <div>
+                            <Link to={`/apod/${apod.date}`} key={idx}>
+                                <Card>
+                                    {mediaDisplay}
+                                    <Card.Body>
+                                        <Card.Title>{apod.title}</Card.Title>
+                                        <Card.Text>{apod.date}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                             </div>
                         )
-
-                        let mediaDisplay;
-
-                        if (apod.media_type === "video"){
-                            mediaDisplay = videoEmbed
-                        } else {
-                            mediaDisplay = <img className="card-image" src={apod.url} alt={apod.title}/>
-                        }
-                            return (
-                                <div>
-                                <Link to={`/apod/${apod.date}`} key={idx}>
-                                    <Card>
-                                        {mediaDisplay}
-                                        <Card.Body>
-                                            <Card.Title>{apod.title}</Card.Title>
-                                            <Card.Text>{apod.date}</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </Link>
-                                </div>
-                            )
-                        }
-                    )
-                }
-            </Masonry> 
+                    }
+                )
+            }
+        </Masonry> 
     )
 }
