94dd08a36212c2ec310d91f411562a6730c8dd72
turekjesse <turekjesse@gmail.com>
2021-07-03 16:52:48 -0400

    copy updated

--- space-cadet/src/components/About/About.js
+++ space-cadet/src/components/About/About.js
@@ -5,12 +5,12 @@ export default function About () {
         <div className="about flex-column">
             <img className="nasa-logo" width="300" alt="NASA Worm logo (gray)" src={NASA_Worm_logo}/>
                 <p className="copyright">Logo Copyright:
-                    <a className="copyright" title="Danne &amp; Blackburn, NY, USA, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:NASA_Worm_logo_(gray).svg">
+                    <a title="Danne &amp; Blackburn, NY, USA, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:NASA_Worm_logo_(gray).svg">
                         <span className="copyright"> Danne &amp; Blackburn, NY, USA, Public domain</span>
                     </a>
                 </p>         
             <div className="explanation">
-                <p>Greetings earthling, and welcome to Space Cadet. This app is a modern take on NASA's infamous <a href="https://apod.nasa.gov/apod/">Astronomy Photo of the Day</a> website, joined with a gallery of images from various Mars exploration missions.</p>
+                <p>Hello and welcome to Space Cadet. This app is a modern take on NASA's infamous <a href="https://apod.nasa.gov/apod/">Astronomy Photo of the Day</a> website, joined with a gallery of images from various Mars exploration missions.</p>
                 <p>Built using React JS and styled with the React Bootstrap and React Masonry CSS libraries.</p>
             </div>
         </div>
